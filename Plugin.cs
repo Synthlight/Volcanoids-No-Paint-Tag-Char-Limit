1fa4e840078cb51afef5a714cfb0f7ff11bbf781
Gregory Brutsche <gregory@mail.brutsches.local>
2021-06-05 13:32:25 -0400

    Initial commit.

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Base_Mod;
+using HarmonyLib;
+using JetBrains.Annotations;
+using TMPro;
+
+namespace No_Paint_Tag_Char_Limit {
+    [UsedImplicitly]
+    public class Plugin : BaseGameMod {
+        protected override string ModName    => "No-Paint-Tag-Char-Limit";
+        protected override bool   UseHarmony => true;
+    }
+
+    [HarmonyPatch]
+    [UsedImplicitly]
+    public static class RemovePaintTagLimitPatch {
+        [HarmonyTargetMethod]
+        [UsedImplicitly]
+        public static MethodBase TargetMethod() {
+            return typeof(PaintCameraController).GetMethod("Awake", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        [UsedImplicitly]
+        [HarmonyPostfix]
+        public static void Postfix(ref TMP_InputField ___m_tag) {
+            ___m_tag.characterLimit = int.MaxValue;
+        }
+    }
+}
