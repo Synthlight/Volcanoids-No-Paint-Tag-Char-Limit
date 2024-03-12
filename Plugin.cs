f9c11c4646d4a897f74098ed395e7f0ac87224d6
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-12 17:35:41 -0500

    Update for Volc 1.31.

--- Plugin.cs
+++ Plugin.cs
@@ -4,25 +4,25 @@ using HarmonyLib;
 using JetBrains.Annotations;
 using TMPro;
 
-namespace No_Paint_Tag_Char_Limit {
+namespace No_Paint_Tag_Char_Limit;
+
+[UsedImplicitly]
+public class Plugin : BaseGameMod {
+    protected override bool UseHarmony => true;
+}
+
+[HarmonyPatch]
+[UsedImplicitly]
+public static class RemovePaintTagLimitPatch {
+    [HarmonyTargetMethod]
     [UsedImplicitly]
-    public class Plugin : BaseGameMod {
-        protected override bool UseHarmony => true;
+    public static MethodBase TargetMethod() {
+        return typeof(PaintCameraController).GetMethod(nameof(PaintCameraController.Awake), BindingFlags.NonPublic | BindingFlags.Instance);
     }
 
-    [HarmonyPatch]
     [UsedImplicitly]
-    public static class RemovePaintTagLimitPatch {
-        [HarmonyTargetMethod]
-        [UsedImplicitly]
-        public static MethodBase TargetMethod() {
-            return typeof(PaintCameraController).GetMethod(nameof(PaintCameraController.Awake), BindingFlags.NonPublic | BindingFlags.Instance);
-        }
-
-        [UsedImplicitly]
-        [HarmonyPostfix]
-        public static void Postfix(ref TMP_InputField ___m_tag) {
-            ___m_tag.characterLimit = int.MaxValue;
-        }
+    [HarmonyPostfix]
+    public static void Postfix(ref TMP_InputField ___m_tag) {
+        ___m_tag.characterLimit = int.MaxValue;
     }
 }
